f167310f99b9c71e846f3fda72ee8588a6ce3ffb
SarboJ <98463276+SarboJ@users.noreply.github.com>
2026-08-18 23:33:59 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
